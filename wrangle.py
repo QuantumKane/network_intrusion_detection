32eec607165a8f5e8bfb9d9952cf88bef41cdb66
Steve Kane <quantumkane00@gmail.com>
2021-04-11 17:26:21 -0500

    added split

--- wrangle.py
+++ wrangle.py
@@ -7,15 +7,13 @@ import sklearn.preprocessing
 from sklearn.model_selection import train_test_split 
 
 
-
 def get_data():    
     '''
-    Read in data from .csv files
+    Read in data from .csv file
     '''
-    train = pd.read_csv("Train_data.csv")
-    test = pd.read_csv("Test_data.csv")
+    df = pd.read_csv("Train_data.csv")
     
-    return train, test
+    return df
 
 
 def wrangle_nids(df):
@@ -26,3 +24,33 @@ def wrangle_nids(df):
     return train
 
 
+def split(df, target):
+    '''
+    this function takes in a dataframe and splits it into 3 samples, 
+    a test, which is 20% of the entire dataframe, 
+    a validate, which is 24% of the entire dataframe,
+    and a train, which is 56% of the entire dataframe. 
+    It then splits each of the 3 samples into a dataframe with independent variables
+    and a series with the dependent, or target variable. 
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test. 
+    '''
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+    
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+    
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+    
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
